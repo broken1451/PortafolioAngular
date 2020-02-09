741fce3a0cb0fd6db4bcf229897cf9337727181c
Adrian bravo <adrianbravo145@gmail.com>
2020-02-09 13:45:09 -0300

    template listo sin rutas

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  public anio: number;
+  constructor() {
+    this.anio = new Date().getFullYear();
+  }
+
+  ngOnInit() {
+  }
+
+}
